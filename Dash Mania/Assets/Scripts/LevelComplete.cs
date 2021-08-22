72e6bd8a06e99f7baaed906557e690b67c7ab29a
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-08-21 23:20:40 -0400

    add menu and credits scenes

--- Dash Mania/Assets/Scripts/LevelComplete.cs
+++ Dash Mania/Assets/Scripts/LevelComplete.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelComplete : MonoBehaviour
-{
-    
+{    
     public void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
